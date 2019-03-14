af1bcba43e019614d01b11c17c716bbad7812534
SYBIT\ssr <stephan.strittmatter@sybit.de>
2019-03-14 11:50:20 +0100

    initial inizalizr project

---
+++ src/main/java/de/schlossgaienhofen/project2019/Project2019Application.java
@@ -0,0 +1,13 @@
+package de.schlossgaienhofen.project2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2019Application.class, args);
+	}
+
+}

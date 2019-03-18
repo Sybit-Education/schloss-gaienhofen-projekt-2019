4c1eed26f17f2f666045364ef24d10a32f061918
Lerch, Nicolaj <Nicolaj.Lerch@sybit.de>
2019-03-18 09:56:59 +0100

    added UserService

---
+++ src/main/java/de/schlossgaienhofen/project2019/service/UserService.java
@@ -0,0 +1,14 @@
+package de.schlossgaienhofen.project2019.service;
+
+import de.schlossgaienhofen.project2019.controller.HomeController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+
+
+}

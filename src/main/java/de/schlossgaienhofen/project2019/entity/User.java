9a7c01613c09e5927d8c38e820041c96ddd1c1a3
Lerch, Nicolaj <Nicolaj.Lerch@sybit.de>
2019-03-18 10:33:04 +0100

    added UserService

--- src/main/java/de/schlossgaienhofen/project2019/entity/User.java
+++ src/main/java/de/schlossgaienhofen/project2019/entity/User.java
@@ -6,47 +6,63 @@ import org.hibernate.annotations.GenericGenerator;
 import javax.persistence.*;
 import java.io.Serializable;
 
-/**
- *
- * @author ahn
- */
 @Entity
-@Table(name = "Users")
-public class User implements Serializable{
+@Table(name = "User")
+public class User implements Serializable {
 
-    @Id
-    @Column(name = "id", updatable = false, nullable = false)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+  @GenericGenerator(name = "native", strategy = "native")
+  @Column(name = "id", updatable = false, nullable = false)
+  private Long id;
 
-    private String nickname;
-    private String fullName;
+  private String firstName;
+  private String name;
+  private String email;
+  private char gender;
 
-    public User() {
 
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public Long getId() {
-        return id;
-    }
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public User() {
 
-    public String getNickname() {
-        return nickname;
-    }
+  }
 
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public String getFullName() {
-        return fullName;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
+  public String getFirstName() {
+    return firstName;
+  }
 
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public char getGender() {
+    return gender;
+  }
+
+  public void setGender(char gender) {
+    this.gender = gender;
+  }
 }

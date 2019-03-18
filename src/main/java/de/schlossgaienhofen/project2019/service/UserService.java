6d4b07c98f8c006fe4501719cbf46c746b5e4155
Lerch, Nicolaj <Nicolaj.Lerch@sybit.de>
2019-03-18 11:00:04 +0100

    added shaPW encryption

--- src/main/java/de/schlossgaienhofen/project2019/service/UserService.java
+++ src/main/java/de/schlossgaienhofen/project2019/service/UserService.java
@@ -1,6 +1,5 @@
 package de.schlossgaienhofen.project2019.service;
 
-import de.schlossgaienhofen.project2019.controller.HomeController;
 import de.schlossgaienhofen.project2019.entity.User;
 import de.schlossgaienhofen.project2019.repository.UserRepository;
 import org.slf4j.Logger;
@@ -9,13 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.NotEmpty;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @Service
 public class UserService {
 
   private final UserRepository userRepository;
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
 
   @Autowired
   public UserService(UserRepository userRepository) {
@@ -28,9 +30,9 @@ public class UserService {
    * @param firstName, name, email
    */
 
-  public void addNewUser(String firstName, String name, String email) {
+  public void addNewUser(String firstName, String name, String email, String password) {
     LOGGER.debug("--> addNewUser");
-    User newUser = createUserObject(firstName, name, email);
+    User newUser = createUserObject(firstName, name, email, password);
     userRepository.save(newUser);
     LOGGER.debug("<-- addNewUser");
   }
@@ -42,12 +44,19 @@ public class UserService {
    * @return user
    */
 
-  private User createUserObject(@NotEmpty String firstName, @NotEmpty String name, @NotEmpty String email) {
+  private User createUserObject(@NotEmpty String firstName, @NotEmpty String name, @NotEmpty String email,
+                                @NotEmpty String password) {
     LOGGER.debug("--> createUserObject");
     User user = new User();
     user.setFirstName(firstName);
     user.setName(name);
     user.setEmail(email);
+
+    try {
+      user.setPassword(getSHA(password));
+    } catch (NoSuchAlgorithmException e) {
+      LOGGER.error("Error getSha" + e);
+    }
     LOGGER.debug("<-- createUserObject");
     return user;
   }
@@ -65,5 +74,26 @@ public class UserService {
     if (user == null) throw new IllegalArgumentException("User is null");
     LOGGER.debug("<-- findUserByEmail");
     return user;
+  }
+
+  /**
+   * Hashes password using sha256 by given password
+   *
+   * @param input
+   * @return
+   */
+
+  public String getSHA(String input) throws NoSuchAlgorithmException {
+    LOGGER.debug("--> getSHA");
+    MessageDigest md = MessageDigest.getInstance("SHA-256");
+    byte[] messageDigest = md.digest(input.getBytes());
+    BigInteger no = new BigInteger(1, messageDigest);
+    StringBuilder hashText = new StringBuilder(no.toString(16));
+    while (hashText.length() < 32) {
+      hashText.insert(0, "0");
+    }
+    LOGGER.debug("<-- getSHA");
+    return hashText.toString();
+
   }
 }
--- src/test/java/de/schlossgaienhofen/project2019/service/UserServiceTest.java
+++ src/test/java/de/schlossgaienhofen/project2019/service/UserServiceTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.security.NoSuchAlgorithmException;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserServiceTest {
@@ -17,8 +19,18 @@ public class UserServiceTest {
 
   @Test
   public void addNewUser() {
-    userService.addNewUser("firstNameTest", "nameTest", "test@mail.com");
+    userService.addNewUser("firstNameTest", "nameTest", "test@mail.com", "testPW");
     User user = userService.findUserByEmail("test@mail.com");
     Assert.assertEquals("nameTest", user.getName());
+  }
+
+  @Test
+  public void getSha() throws NoSuchAlgorithmException {
+
+    String input = "Sybit2019Gaienhofen";
+    String output = "e07de83e6058810e2bf9e6fafe86237d7b903f889c667b84e0639989dfceebb6";
+
+    Assert.assertEquals(output, userService.getSHA(input));
+
   }
 }

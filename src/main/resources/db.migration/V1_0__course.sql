13fff1a4b60b696cb85f8f84bfb972da860beb00
Suppa <dsa@sybit.de>
2019-03-18 10:32:38 +0100

    added database table user and course

---
+++ src/main/resources/db.migration/V1_0__course.sql
@@ -0,0 +1,10 @@
+CREATE TABLE course (
+    id bigint NOT NULL AUTO_INCREMENT,
+    course_name varchar(50),
+);
+
+INSERT INTO course (id, course_name) VALUES (1, 'Schwimm-Kurs');
+INSERT INTO course (id, course_name) VALUES (2, 'Lauf-Kurs');
+INSERT INTO course (id, course_name) VALUES (3, 'Mathe Nachhilfe-Kurs');
+INSERT INTO course (id, course_name) VALUES (4, 'Selbstverteidigungs-Kurs');
+INSERT INTO course (id, course_name) VALUES (5, 'Reit-Kurs');

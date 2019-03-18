9f26989061100e587e0e43fffcbe7dd491625a9c
cwr <Christian.Wolter@sybit.de>
2019-03-18 11:55:08 +0100

    Register

--- src/main/java/de/schlossgaienhofen/project2019/controller/LoginController.java
+++ src/main/java/de/schlossgaienhofen/project2019/controller/LoginController.java
@@ -1,14 +1,18 @@
 package de.schlossgaienhofen.project2019.controller;
 
+import de.schlossgaienhofen.project2019.entity.User;
+import de.schlossgaienhofen.project2019.repository.UserRepository;
 import de.schlossgaienhofen.project2019.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/login")
@@ -17,9 +21,11 @@ public class LoginController {
   private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
 
   private final UserService userService;
+  private final UserRepository userRepository;
 
-  public LoginController(UserService userService) {
+  public LoginController(UserService userService, UserRepository userRepository) {
     this.userService = userService;
+    this.userRepository = userRepository;
   }
 
   /**
@@ -36,23 +42,25 @@ public class LoginController {
   }
 
   @GetMapping(value = "/register")
-  public String register() {
-    return "register";
+  public ModelAndView displayRegister(ModelAndView modelAndView, User user)  {
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("register");
+        return modelAndView;
   }
 
   @PostMapping(value = "/register")
-  public String register(@RequestParam("firstName") String firstName,
-                         @RequestParam("name") String name,
-                         @RequestParam("email") String email,
-                         @RequestParam("password") String password,
-                         @RequestParam("confirmPassword") String confirmPassword) {
+  public ModelAndView registerUser(ModelAndView modelAndView, User user) {
 
     LOGGER.debug("--> register");
-
-    userService.addNewUser(firstName, name, email, password);
+    
+    if(userRepository.findByEmail(user.getEmail()) != null){
+        modelAndView.addObject("message","This email already exists!");
+        modelAndView.setViewName("error");
+    }
+    userService.addNewUser(user);
 
     LOGGER.debug("<-- register");
-    return "login";
+    return modelAndView;
   }
 
 }

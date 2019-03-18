dd12c83c041e5a0cb809a3d2e9d8fce5669bf48f
Lerch, Nicolaj <Nicolaj.Lerch@sybit.de>
2019-03-18 14:58:10 +0100

    test master push

--- src/main/java/de/schlossgaienhofen/project2019/service/SMTPServerService.java
+++ src/main/java/de/schlossgaienhofen/project2019/service/SMTPServerService.java
@@ -1,4 +1,3 @@
-
 package de.schlossgaienhofen.project2019.service;
 
 import org.slf4j.Logger;
--- src/main/java/de/schlossgaienhofen/project2019/service/SimpleMessageListenerImpl.java
+++ src/main/java/de/schlossgaienhofen/project2019/service/SimpleMessageListenerImpl.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.schlossgaienhofen.project2019.service;
 
 import java.io.InputStream;
@@ -10,18 +5,15 @@ import java.util.Properties;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.subethamail.smtp.helper.SimpleMessageListener;
 
-/**
- *
- * @author ssr
- */
 public class SimpleMessageListenerImpl implements SimpleMessageListener {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(SimpleMessageListenerImpl.class);
-  
+
   public SimpleMessageListenerImpl() {
   }
 
@@ -37,7 +29,7 @@ public class SimpleMessageListenerImpl implements SimpleMessageListener {
       MimeMessage m = new MimeMessage(session, data);
       LOGGER.info("Mail deliver: " + m.getSubject());
       // ... here we go with email message ...
-      
+
     } catch (MessagingException ex) {
       LOGGER.error(ex.getMessage(), ex);
     }

7d73784dd41c630bd67d7819d543e1c4b785d164
Lerch, Nicolaj <Nicolaj.Lerch@sybit.de>
2019-03-18 11:36:27 +0100

    merged registerController into loginController

--- src/main/java/de/schlossgaienhofen/project2019/controller/LoginController.java
+++ src/main/java/de/schlossgaienhofen/project2019/controller/LoginController.java
@@ -1,16 +1,26 @@
 package de.schlossgaienhofen.project2019.controller;
 
+import de.schlossgaienhofen.project2019.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(value = "/login")
 public class LoginController {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
+
+  private final UserService userService;
+
+  public LoginController(UserService userService) {
+    this.userService = userService;
+  }
 
   /**
    * Mapping for loginPage
@@ -22,6 +32,25 @@ public class LoginController {
   public String login() {
     LOGGER.debug("--> login");
     LOGGER.debug("<-- login");
+    return "login";
+  }
+
+  @GetMapping(value = "/register")
+  public String register() {
+    return "register";
+  }
+
+  @PostMapping(value = "/register")
+  public String register(@RequestParam("vorName") String firstName,
+                         @RequestParam("nachName") String name,
+                         @RequestParam("email") String email,
+                         @RequestParam("password") String password) {
+
+    LOGGER.debug("--> register");
+
+    userService.addNewUser(firstName, name, email, password);
+
+    LOGGER.debug("<-- register");
     return "login";
   }
 

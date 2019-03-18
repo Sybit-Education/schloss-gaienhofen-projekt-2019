da83282392986279e79317231b29284728840e8e
cwr <Christian.Wolter@sybit.de>
2019-03-18 11:23:04 +0100

    Register

--- src/main/java/de/schlossgaienhofen/project2019/controller/RegisterController.java
+++ src/main/java/de/schlossgaienhofen/project2019/controller/RegisterController.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,7 +18,7 @@ public class RegisterController {
     @Autowired
     UserService userService;
 
-    @GetMapping
+    @PostMapping
     public String register(@RequestParam("vorName") String firstName,
             @RequestParam("nachName") String name,
             @RequestParam("email") String email,

b53a2695e165ae440541e1ff52a1d6430b95e147
DavKat <daka01@gmx.net>
2019-03-25 18:24:59 +0100

    Added Activity Group List

--- src/main/java/de/schlossgaienhofen/project2019/controller/HomeController.java
+++ src/main/java/de/schlossgaienhofen/project2019/controller/HomeController.java
@@ -1,7 +1,9 @@
 package de.schlossgaienhofen.project2019.controller;
 
 
+import de.schlossgaienhofen.project2019.entity.ActivityGroup;
 import de.schlossgaienhofen.project2019.entity.Course;
+import de.schlossgaienhofen.project2019.service.ActivityGroupService;
 import de.schlossgaienhofen.project2019.service.CourseService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,10 +22,15 @@ public class HomeController {
   private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
 
   private final CourseService courseService;
+  
+  private final ActivityGroupService activityGroupService;
 
-  public HomeController(CourseService courseService) {
+  public HomeController(CourseService courseService, ActivityGroupService activityGroupService) {
     this.courseService = courseService;
+    this.activityGroupService = activityGroupService;
   }
+  
+ 
 
   /**
    * Shows all WeeklyReports on index
@@ -33,10 +40,12 @@ public class HomeController {
    */
   @GetMapping(value = "/")
   public String viewHome(Map<String, Object> model) {
-    LOGGER.debug("--> viewHome");
-    List<Course> allCourseList = courseService.getAllCourses();
-    model.put("allCourse", allCourseList);
-    LOGGER.debug("<-- viewHome");
+    LOGGER.debug("----->viewHome");
+	List<ActivityGroup> allActivityGroups = activityGroupService.getAllActivityGroups();
+    model.put("allActivities", allActivityGroups);
+    LOGGER.debug("<-----viewHome");
     return "index";
   }
+  
+  
 }

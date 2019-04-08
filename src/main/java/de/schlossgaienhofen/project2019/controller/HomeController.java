3b6aabce9f963c7adba5dbd0f0af71a90bb9c14e
DavKat <daka01@gmx.net>
2019-04-08 16:51:56 +0200

    added creator page to HomeController

--- src/main/java/de/schlossgaienhofen/project2019/controller/HomeController.java
+++ src/main/java/de/schlossgaienhofen/project2019/controller/HomeController.java
@@ -5,6 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import de.schlossgaienhofen.project2019.entity.User;
+import de.schlossgaienhofen.project2019.service.ActivityGroupService;
 
 import java.util.List;
 import java.util.Map;
@@ -17,9 +21,10 @@ public class HomeController {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
 
-
-  public HomeController() {
-
+  private final ActivityGroupService activityGroupService;
+  
+  public HomeController(ActivityGroupService activityGroupService) {
+	 this.activityGroupService = activityGroupService;
   }
 
   /**
@@ -31,9 +36,13 @@ public class HomeController {
   @GetMapping(value = "/")
   public String viewHome(Map<String, Object> model) {
     LOGGER.debug("--> viewHome");
-
-
     LOGGER.debug("<-- viewHome");
     return "index";
+  }
+ 
+  @GetMapping(value = "/create")
+  public ModelAndView displayRegister(ModelAndView modelAndView) {
+    modelAndView.setViewName("create");
+    return modelAndView;
   }
 }

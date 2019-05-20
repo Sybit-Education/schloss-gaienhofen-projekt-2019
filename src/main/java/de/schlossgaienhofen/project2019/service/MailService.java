d8e48a06a8bfcbc4a372a091409e7babd16155e3
Lerch, Nicolaj <Nicolaj.Lerch@sybit.de>
2019-05-20 08:32:20 +0200

    code optimized

--- src/main/java/de/schlossgaienhofen/project2019/service/MailService.java
+++ src/main/java/de/schlossgaienhofen/project2019/service/MailService.java
@@ -2,6 +2,7 @@ package de.schlossgaienhofen.project2019.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -10,11 +11,9 @@ import org.springframework.stereotype.Service;
 public class MailService {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(MailService.class);
-  public JavaMailSender emailSender;
 
-  public MailService(JavaMailSender emailSender) {
-    this.emailSender = emailSender;
-  }
+  @Autowired
+  private JavaMailSender emailSender;
 
   /**
    * Send Mail

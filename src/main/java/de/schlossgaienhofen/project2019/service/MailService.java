78a9c60de61ca5f05870dbbe2bf2880d62da5e81
Lerch, Nicolaj <Nicolaj.Lerch@sybit.de>
2019-05-20 13:45:10 +0200

    added mailService interface

--- src/main/java/de/schlossgaienhofen/project2019/service/MailService.java
+++ src/main/java/de/schlossgaienhofen/project2019/service/MailService.java
@@ -1,19 +1,6 @@
 package de.schlossgaienhofen.project2019.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
-
-@Service
-public class MailService {
-
-  private static final Logger LOGGER = LoggerFactory.getLogger(MailService.class);
-
-  @Autowired
-  private JavaMailSender emailSender;
+public interface MailService {
 
   /**
    * Send Mail
@@ -22,13 +9,6 @@ public class MailService {
    * @param subject
    * @param text
    */
-  public void sendSimpleMessage(String to, String subject, String text) {
-    LOGGER.debug("--> sendSimpleMessage");
-    SimpleMailMessage message = new SimpleMailMessage();
-    message.setTo(to);
-    message.setSubject(subject);
-    message.setText(text);
-    emailSender.send(message);
-    LOGGER.debug("<-- sendSimpleMessage");
-  }
+
+  void sendSimpleMessage(String to, String subject, String text);
 }

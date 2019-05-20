c3b4fcdb6a1ced882f0a001163d59cfe609196ae
ssr <stephan.strittmatter@sybit.de>
2019-05-20 14:13:50 +0200

    Remove Register

--- src/main/java/de/schlossgaienhofen/project2019/controller/LoginController.java
+++ src/main/java/de/schlossgaienhofen/project2019/controller/LoginController.java
@@ -18,12 +18,6 @@ public class LoginController {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
 
-  @Autowired
-  private UserService userService;
-
-  @Autowired
-  private UserRepository userRepository;
-
   /**
    * Mapping for loginPage
    *
@@ -35,30 +29,6 @@ public class LoginController {
     LOGGER.debug("--> login");
     LOGGER.debug("<-- login");
     return "login";
-  }
-
-  @GetMapping(value = "/register")
-  public ModelAndView displayRegister(ModelAndView modelAndView, User user) {
-    modelAndView.addObject("user", user);
-    modelAndView.setViewName("register");
-    return modelAndView;
-  }
-
-  @PostMapping(value = "/register")
-  public ModelAndView registerUser(ModelAndView modelAndView, User user) {
-
-    LOGGER.debug("--> register");
-    modelAndView.setViewName("register");
-    if (userRepository.findByEmail(user.getEmail()) != null) {
-      modelAndView.addObject("message", "This email already exists!");
-      modelAndView.setViewName("error");
-    }
-    userService.addNewUser(user);
-
-    modelAndView.setViewName("login");
-
-    LOGGER.debug("<-- register");
-    return modelAndView;
   }
 
 }

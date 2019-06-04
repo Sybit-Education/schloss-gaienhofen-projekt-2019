a2ed2ef2e632376c12e5eaa3265896f9438714ac
Wolter, Christian <Christian.Wolter@sybit.de>
2019-06-04 14:40:57 +0200

    added email yml

--- src/main/java/de/schlossgaienhofen/project2019/service/MailServiceImpl.java
+++ src/main/java/de/schlossgaienhofen/project2019/service/MailServiceImpl.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Service
 public class MailServiceImpl implements MailService {

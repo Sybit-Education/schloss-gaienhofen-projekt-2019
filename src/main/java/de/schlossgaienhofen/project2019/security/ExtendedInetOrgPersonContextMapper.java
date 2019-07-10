480f078d852a5d70b3d4f92e917165a2f530bd76
SYBIT\ssr <stephan.strittmatter@sybit.de>
2019-07-10 10:39:22 +0200

    Authentication aof teachers solved

---
+++ src/main/java/de/schlossgaienhofen/project2019/security/ExtendedInetOrgPersonContextMapper.java
@@ -0,0 +1,26 @@
+package de.schlossgaienhofen.project2019.security;
+
+import org.springframework.ldap.core.DirContextOperations;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.ldap.userdetails.InetOrgPerson;
+import org.springframework.security.ldap.userdetails.InetOrgPersonContextMapper;
+
+import java.util.Collection;
+
+public class ExtendedInetOrgPersonContextMapper extends InetOrgPersonContextMapper {
+
+  public UserDetails mapUserFromContext(DirContextOperations ctx, String username,
+                                        Collection<? extends GrantedAuthority> authorities) {
+    InetOrgPerson.Essence p = new InetOrgPerson.Essence(ctx);
+
+    p.setUsername(username);
+    p.setAuthorities(authorities);
+    p.setMail(username);
+    p.setGivenName(ctx.getStringAttribute("givenName"));
+    p.setSn(ctx.getStringAttribute("sn"));
+
+    return p.createUserDetails();
+
+  }
+}
